e37168311325cfd7b3d058003d29d299de53f2fb
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-08-18 13:04:36 +0200

    all test done

--- src/test/add_sub_scale.rs
+++ src/test/add_sub_scale.rs
@@ -90,4 +90,19 @@ mod tests {
         assert_eq!(v2.sub(&v1), Vector {v: vec![4, 4, 4, 4]});
         assert_eq!(mv1.sub(&mv2), Matrix { data: vec![Vector {v: vec![0, 0, 0, 0]}, Vector {v: vec![0, 0, 0, 0]}], col: 2, row: 1 });
     }
+
+    #[test]
+    fn scale() {
+        let m1 = Matrix {data: vec![1, 2, 3, 4], col: 2, row: 2};
+        let m2 = Matrix {data: vec![5, 6, 7, 8], col: 2, row: 2};
+        let v1 = Vector {v: vec![1.5, 2.5, 3.5, 4.5]};
+        let v2 = Vector {v: vec![5.5, 6.5, 7.5, 8.5]};
+        let mv1 = Matrix {data: vec![Vector{v: vec![1.0, 2.0, 3.0, 4.0]}, Vector{v: vec![5.0, 6.0, 7.0, 8.0]}], col: 2, row: 1};
+        assert_eq!(m1.scale(&2), Matrix {data: vec![2, 4, 6, 8], col: 2, row: 2});
+        assert_eq!(m2.scale(&-3), Matrix {data: vec![-15, -18, -21, -24], col: 2, row: 2});
+        assert_eq!(v1.scale(&2.0), Vector {v: vec![3.0, 5.0, 7.0, 9.0]});
+        assert_eq!(v2.scale(&-1.5), Vector {v: vec![-8.25, -9.75, -11.25, -12.75]});
+        assert_eq!(mv1.scale(&Vector {v: vec![1.0, 2.0, 3.0, 4.0]}), Matrix { data: vec![Vector {v: vec![1.0, 2.0, 3.0, 4.0]}, Vector {v: vec![5.0, 6.0, 7.0, 8.0]}], col: 2, row: 1 });
+    }
+
 }
