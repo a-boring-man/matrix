b3e7f91fb0947723c7aa556f19186ce539cc9eab
42 <jonathan.rinna@gmail.com>
2023-08-19 20:07:29 +0200

    linear

---
+++ src/matrix/linear_combination.rs
@@ -0,0 +1,8 @@
+use super::definition::*;
+use super::add_sub_scale::*;
+
+impl<K: Scalar> Matrix<K> {
+    pub fn linear_combination(u : &[Vector<K>], coefs: &[K]) -> Vector<K> {
+        
+    }
+}
--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,2 +1,3 @@
 pub mod definition;
 pub mod add_sub_scale;
+pub mod linear_combination;
