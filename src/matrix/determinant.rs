80d7f94182a07e742f7c9d9b5de43c6f782c22ac
jonathan <jonathan.rinna@gmail.com>
2023-08-23 20:49:35 +0200

    det_3d

--- src/matrix/determinant.rs
+++ src/matrix/determinant.rs
@@ -8,4 +8,12 @@ impl<K: Scalar> Matrix<K> where for<'a> &'a K: Mul<&'a K, Output = K>{
 		}
 		&self.data[0] * &self.data[2] - &self.data[1] * &self.data[3]
 	}
+
+	pub fn determinant_3d(&self) -> K {
+		if self.data.len() != 9 || !self.is_square() {
+			panic!("wrong matrix dimension in determinant calculation");
+		}
+		let m = &self.data;
+		&(&m[0] * &m[4]) * &m[8] + &(&m[1] * &m[5]) * &m[6] + &(&m[2] * &m[3]) * &m[7] - &(&m[2] * &m[4]) * &m[6] - &(&m[1] * &m[3]) * &m[8] - &(&m[0] * &m[5]) * &m[7]
+	}
 }
