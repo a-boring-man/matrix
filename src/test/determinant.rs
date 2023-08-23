60662137ee388593e86b219b8d7a318af857ea7c
jonathan <jonathan.rinna@gmail.com>
2023-08-23 20:52:06 +0200

    3d determinant fail

--- src/test/determinant.rs
+++ src/test/determinant.rs
@@ -3,8 +3,11 @@ mod test {
     use crate::matrix::definition::Matrix;
 
 	#[test]
-	fn determinant_2d() {
+	fn determinant() {
 		let m1 = Matrix::from((vec![1, 2, 3, 4], 2, 2));
 		assert_eq!(-5, m1.determinant_2d());
+
+		let m2 = Matrix::from((vec![1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3));
+		assert_eq!(45, m2.determinant_3d());
 	}
 }
