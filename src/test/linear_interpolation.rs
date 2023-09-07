032a2782d900d93ccf4dab72430cc650ff66dad6
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-09-07 10:47:54 +0200

    complex 3/13

--- src/test/linear_interpolation.rs
+++ src/test/linear_interpolation.rs
@@ -3,12 +3,14 @@ mod test {
 
 	use assert_approx_eq::assert_approx_eq;
 
-	use crate::matrix::basic_definition::definition::{Vector, Matrix};
+	use crate::matrix::basic_definition::{definition::{Vector, Matrix}, complex::Complex};
 
 	#[test]
 	fn linear_interpolation() {
 		let v1 = Vector::from(vec![1.0, 4.0]);
 		let v2 = Vector::from(vec![5.0, 16.0]);
+		let v3 = Vector::from(vec![Complex{re: 1.0, im: 4.0}, Complex{re: -6., im: 3.}]);
+		let v4 = Vector::from(vec![Complex{re: 5., im: 2.}, Complex{re: 2.0, im: 5.0}]);
 
 		let m1 = Matrix::from((vec![10.0, -20.0, 4.0, -3.0], 2, 2));
 		let m2 = Matrix::from((vec![25.0, 42.0, 64.0, -7.0], 2, 2));
@@ -16,6 +18,11 @@ mod test {
 		assert_eq!(Vector::from(vec![3.0, 10.0]), Vector::lerp(&v1, &v2, &0.5));
 		assert_eq!(Vector::from(vec![1.0, 4.0]), Vector::lerp(&v1, &v2, &0.0));
 		assert_eq!(Vector::from(vec![5.0, 16.0]), Vector::lerp(&v1, &v2, &1.0));
+
+		assert_eq!(Vector::from(vec![Complex{re: 3., im: 3.0}, Complex{re: -2., im: 4.}]), Vector::lerp(&v3, &v4, &Complex{re: 0.5, im: 0.0}));
+		assert_eq!(Vector::from(vec![Complex{re: 1.0, im: 4.0}, Complex{re: -6., im: 3.}]), Vector::lerp(&v3, &v4, &Complex{re: 0.0, im: 0.0}));
+		assert_eq!(Vector::from(vec![Complex{re: 5., im: 2.}, Complex{re: 2.0, im: 5.0}]), Vector::lerp(&v3, &v4, &Complex{re: 1.0, im: 0.0}));
+
 		assert_eq!(Matrix::from((vec![10.0, -20.0, 4.0, -3.0], 2, 2)), Matrix::lerp(&m1, &m2, 0.0));
 		assert_eq!(Matrix::from((vec![25.0, 42.0, 64.0, -7.0], 2, 2)), Matrix::lerp(&m1, &m2, 1.0));
 		let lerp = Matrix::lerp(&m1, &m2, 0.3);
