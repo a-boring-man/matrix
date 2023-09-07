7d57c9658418beadea6db76bb158a83094d45036
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-09-07 17:51:13 +0200

    7/13

--- src/matrix/vector_impl/cross_product.rs
+++ src/matrix/vector_impl/cross_product.rs
@@ -1,14 +1,12 @@
-use std::ops::Mul;
-
 use crate::matrix::basic_definition::{trait_definition::Scalar, definition::Vector};
 
-impl<K: Scalar> Vector<K> where for<'a> &'a K: Mul<&'a K, Output = K>{
+impl<K: Scalar> Vector<K> {
 	pub fn cross_product(u: &Vector<K>, v: &Vector<K>) -> Vector<K> {
 		if u.v.len() != 3 || v.v.len() != 3 {
 			panic!("cross product call on a non 3d vector");
 		}
 		Vector {
-			v: vec![&u.v[1] * &v.v[2] - &u.v[2] * &v.v[1], &u.v[2] * &v.v[0] - &u.v[0] * &v.v[2], &u.v[0] * &v.v[1] - &u.v[1] * &v.v[0]]
+			v: vec![u.v[1] * v.v[2] - u.v[2] * v.v[1], u.v[2] * v.v[0] - u.v[0] * v.v[2], u.v[0] * v.v[1] - u.v[1] * v.v[0]]
 		}
 	}
 }
--- src/test/cross_product.rs
+++ src/test/cross_product.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-    use crate::matrix::basic_definition::definition::Vector;
+    use crate::matrix::basic_definition::{definition::Vector, complex::Complex};
 
 	#[test]
 	fn cross_product() {
@@ -15,5 +15,9 @@ mod test {
 		let u1 = Vector::from(vec![4.0, 2.0, -3.0]);
 		let v1 = Vector::from(vec![-2.0, -5.0, 16.0]);
 		assert_eq!(Vector::from(vec![17.0, -58.0, -16.0]), Vector::cross_product(&u1, &v1));
+
+		let v9 = Vector::from(vec![Complex{re: 1., im: 4.}, Complex{re: 3., im: 9.}, Complex{re: 5., im: 3.}]);
+		let v10 = Vector::from(vec![Complex{re : 2., im: 5.}, Complex{re: -2., im: 4.}, Complex{re: 4., im: 7.}]);
+		assert_eq!(Vector::from(vec![Complex{re: -29., im: 43.}, Complex{re: 19., im: 8.}, Complex{re: 21., im: -37.}]), Vector::cross_product(&v9, &v10))
 	}
 }
