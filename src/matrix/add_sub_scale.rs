2042efb2d9e21fa45d2e632cafedbfe667c418b3
42 <jonathan.rinna@gmail.com>
2023-09-12 19:13:32 +0200

    basic add sub and mul impl

--- src/matrix/add_sub_scale.rs
+++ src/matrix/add_sub_scale.rs
@@ -1,6 +1,6 @@
 use std::{fmt::Display, ops::{Add, Sub, Mul, AddAssign}};
 
-use super::basic_definition::{trait_definition::Scalar, definition::{Matrix, Vector, matrix}};
+use super::basic_definition::{trait_definition::Scalar, definition::{Matrix, Vector, matrix, vector}};
 
 impl<K : Scalar + Display> Matrix<K> {
     pub fn self_add(&mut self, m: &Matrix<K>) {
@@ -51,69 +51,7 @@ impl<K : Scalar + Display> Matrix<K> {
     }
 }
 
-impl<K: Add<Output = K> + Copy, const R: usize, const C: usize> AddAssign for matrix<K, R, C> {
-    
-    fn add_assign(&mut self, rhs: Self) {
-        self
-            .iter_mut()
-            .zip(rhs.iter())
-            .for_each(|(vec1, vec2)| {
-                vec1
-                    .iter_mut()
-                    .zip(vec2.iter())
-                    .for_each(|(v1, v2)| *v1 = *v1 + *v2);
-        })
-    }
-}
-
-impl<K: Default + Copy + Add<Output = K>, const R: usize, const C: usize> Add for matrix<K, R, C> {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        let mut e = [[K::default(); C]; R];
-        for r in 0..R {
-            for c in 0..C {
-                unsafe {
-                    *e.get_unchecked_mut(r).get_unchecked_mut(c) = 
-                        *self.e.get_unchecked(r).get_unchecked(c) 
-                        + *rhs.e.get_unchecked(r).get_unchecked(c);
-                }
-            }
-        }
-        matrix { e }
-    }
-}
-
-impl<K: Default + Copy + Sub<Output = K>, const R: usize, const C: usize> Sub for matrix<K, R, C> {
-    type Output = Self;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        let mut e = [[K::default(); C]; R];
-        for r in 0..R {
-            for c in 0..C {
-                unsafe {
-                    *e.get_unchecked_mut(r).get_unchecked_mut(c) = 
-                        *self.e.get_unchecked(r).get_unchecked(c) 
-                        - *rhs.e.get_unchecked(r).get_unchecked(c);
-                }
-            }
-        }
-        matrix { e }
-    }
-}
-
-impl<K: Default + Copy + Mul<Output = K>, const R: usize, const C: usize> Mul<K> for matrix<K, R, C> {
-    type Output = Self;
-
-    fn mul(self, rhs: K) -> Self::Output {
-        let mut e = [[K::default(); C]; R];
-        e.iter_mut().for_each(|row| row.iter_mut().for_each(|e| *e = *e * rhs));
-        matrix { e }
-    }
-}  
-
-impl<K: Scalar> Vector<K>
-{
+impl<K: Scalar> Vector<K> {
     pub fn self_add(&mut self, v: &Vector<K>) {
         if self.v.len() != v.v.len() {
             panic!("dimension error on vector addition");}
@@ -150,5 +88,104 @@ impl<K: Scalar> Vector<K>
         Vector {
             v: self.iter().map(|e| e.clone() * a.clone()).collect::<Vec<_>>(),
         } 
+    }
+}
+
+impl<K: Add<Output = K> + Copy, const R: usize, const C: usize> AddAssign for matrix<K, R, C> {
+    
+    fn add_assign(&mut self, rhs: Self) {
+        self
+            .iter_mut()
+            .zip(rhs.iter())
+            .for_each(|(vec1, vec2)| {
+                vec1
+                    .iter_mut()
+                    .zip(vec2.iter())
+                    .for_each(|(v1, v2)| *v1 = *v1 + *v2);
+        })
+    }
+}
+impl<K: Default + Copy + Add<Output = K>, const R: usize, const C: usize> Add for matrix<K, R, C> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut e = [[K::default(); C]; R];
+        for r in 0..R {
+            for c in 0..C {
+                unsafe {
+                    *e.get_unchecked_mut(r).get_unchecked_mut(c) = 
+                        *self.e.get_unchecked(r).get_unchecked(c) 
+                        + *rhs.e.get_unchecked(r).get_unchecked(c);
+                }
+            }
+        }
+        matrix { e }
+    }
+}
+impl<K: Default + Copy + Sub<Output = K>, const R: usize, const C: usize> Sub for matrix<K, R, C> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut e = [[K::default(); C]; R];
+        for r in 0..R {
+            for c in 0..C {
+                unsafe {
+                    *e.get_unchecked_mut(r).get_unchecked_mut(c) = 
+                        *self.e.get_unchecked(r).get_unchecked(c) 
+                        - *rhs.e.get_unchecked(r).get_unchecked(c);
+                }
+            }
+        }
+        matrix { e }
+    }
+}
+impl<K: Default + Copy + Mul<Output = K>, const R: usize, const C: usize> Mul<K> for matrix<K, R, C> {
+    type Output = Self;
+
+    fn mul(self, rhs: K) -> Self::Output {
+        let mut e = [[K::default(); C]; R];
+        e.iter_mut().for_each(|row| row.iter_mut().for_each(|e| *e = *e * rhs));
+        matrix { e }
+    }
+}
+
+impl<K: Add<Output = K> + Copy, const L: usize> AddAssign for vector<K, L> {
+    fn add_assign(&mut self, rhs: Self) {
+        self.iter_mut().zip(rhs.iter()).for_each(|(v1, v2)| *v1 = *v1 + *v2);
+    }
+}
+impl<K: Default + Copy + Add<Output = K>, const L: usize> Add for vector<K, L> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut e = [K::default(); L];
+        for i in 0..L {
+            unsafe {
+                *e.get_unchecked_mut(i) = *self.e.get_unchecked(i) + *rhs.e.get_unchecked(i);
+            }
+        }
+        vector { e }
+    }
+}
+impl<K: Default + Copy + Sub<Output = K>, const L: usize> Sub for vector<K, L> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut e = [K::default(); L];
+        for i in 0..L {
+            unsafe {
+                *e.get_unchecked_mut(i) = *self.e.get_unchecked(i) - *rhs.e.get_unchecked(i);
+            }
+        }
+        vector { e }
+    }
+}
+impl<K: Default + Copy + Mul<Output = K>, const L: usize> Mul<K> for vector<K, L> {
+    type Output = Self;
+
+    fn mul(self, rhs: K) -> Self::Output {
+        let mut e = [K::default(); L];
+        e.iter_mut().for_each(|e| *e = *e * rhs);
+        vector { e }
     }
 }
